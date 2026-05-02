51ed6c687e7435159d5c5931e129335995ddf5eb
Javid <118032087+AstredStar@users.noreply.github.com>
2026-05-01 23:17:41 -0400

    finishing practice

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 import "./styles.css"
 import { greeting } from "./greeting.js";
+import logo from "./images/logo.svg"
 
 console.log(greeting);
+const image = document.createElement("img");
+image.src = logo;
+document.body.appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,39 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    
-  mode: "development",
-  entry: "./src/script.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
+    mode: "development",
+    entry: "./src/script.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
     ],
-  },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 };
